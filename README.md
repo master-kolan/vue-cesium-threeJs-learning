fa5e9c9bdff2fe51a7d884f42fb95f35f0f2d99e
zhichao.feng <365707730@qq.com>
2020-09-25 10:09:08 +0800

    initial build branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-cesium-threeJs-learning
-使用 vue cli3 + cesium + threeJs 学习的例子
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'cesium/Widgets/widgets.css'
-
-
-createApp(App).mount('#app')
